5d26f3fce7ce8d2c43b4f01242fff3b1708828e8
Brian Michel <brian.michel@gmail.com>
2022-11-18 17:22:51 -0500

    Initial Commit

---
+++ ddd/dddApp.swift
@@ -0,0 +1,17 @@
+//
+//  dddApp.swift
+//  ddd
+//
+//  Created by Brian Michel on 11/18/22.
+//
+
+import SwiftUI
+
+@main
+struct dddApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ dddUITests/dddUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  dddUITestsLaunchTests.swift
+//  dddUITests
+//
+//  Created by Brian Michel on 11/18/22.
+//
+
+import XCTest
+
+final class dddUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
